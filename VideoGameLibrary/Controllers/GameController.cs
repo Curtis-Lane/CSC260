3a4ba3c2685c5dc608bd05a15d7b8de04991f389
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-01-18 11:51:35 -0700

    Finish the Video Game Library Part 1 Lab

--- VideoGameLibrary/Controllers/GameController.cs
+++ VideoGameLibrary/Controllers/GameController.cs
@@ -5,7 +5,7 @@ namespace VideoGameLibrary.Controllers {
 	public class GameController : Controller {
 		private static List<VideoGame> videoGames = new List<VideoGame>{
 			new VideoGame("Team Fortress 2", "PC", "FPS", "M", 2007, "https://upload.wikimedia.org/wikipedia/en/5/5f/Tf2_standalonebox.jpg"),
-			new VideoGame("Deep Rock Galactic", "PC", "FPS", "T", 2020, "https://upload.wikimedia.org/wikipedia/en/b/b3/Deep_rock_galactic_cover_art.jpg"),
+			new VideoGame("Deep Rock Galactic", "PC, Xbox Series, Playstation 5", "FPS", "T", 2020, "https://upload.wikimedia.org/wikipedia/en/b/b3/Deep_rock_galactic_cover_art.jpg"),
 			new VideoGame("Payday 2", "PC", "FPS", "M", 2013, "https://upload.wikimedia.org/wikipedia/en/7/7b/Payday2cover.jpg"),
 			new VideoGame("Terraria", "PC, Xbox Series, Playstation 5, Mobile", "Side-scroller", "T", 2011, "https://upload.wikimedia.org/wikipedia/en/1/1a/Terraria_Steam_artwork.jpg"),
 			new VideoGame("Grand Theft Auto V", "PC, Xbox Series, Playstation 5", "FPS", "M", 2013, "https://upload.wikimedia.org/wikipedia/en/a/a5/Grand_Theft_Auto_V.png")
