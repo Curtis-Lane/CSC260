559d619aa34d1592976da6503b49b91f82ea30d4
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-01-23 19:45:55 -0700

    Finish(?) the Routing Exercise assignment

--- Routing/Controllers/HomeController.cs
+++ Routing/Controllers/HomeController.cs
@@ -18,7 +18,31 @@ namespace Routing.Controllers {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Chicken() {
+            return Redirect("https://www.chick-fil-a.com");
+        }
+
+        public IActionResult DefaultCow(int mooCount) {
+            return Content("The cow Default Cow moos at you " + mooCount.ToString() + " times.");
+        }
+
+        public IActionResult NamedCow(int mooCount, string cowName) {
+            return Content("The cow " + cowName + " moos at you " + mooCount.ToString() + " times.");
+        }
+
+        public IActionResult AllCowsCount(int cowCount) {
+            return Content("There are " + cowCount.ToString() + " Cows on page");
+        }
+
+		public IActionResult AllCowsCountPerPage(int cowCount, int pageNumber) {
+			return Content("There are " + cowCount.ToString() + " cows per page, on page " + pageNumber.ToString());
+		}
+
+		public IActionResult AllCowsCountOnPage(int cowCount, int pageNumber) {
+			return Content("There are " + cowCount.ToString() + " Cows on page " + pageNumber.ToString());
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
--- Routing/Program.cs
+++ Routing/Program.cs
@@ -1,28 +1,71 @@
 namespace Routing {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program {
+		public static void Main(string[] args) {
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if(!app.Environment.IsDevelopment()) {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
+			// Configure the HTTP request pipeline.
+			if(!app.Environment.IsDevelopment()) {
+				app.UseExceptionHandler("/Home/Error");
+			}
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Privacy}/{id?}"
+			);
 
-            app.Run();
-        }
-    }
+			app.MapControllerRoute(
+				name: "Chicken",
+				pattern: "/EatMoreChicken",
+				defaults: new {controller="Home", action="Chicken"}
+			);
+
+			app.MapControllerRoute(
+				name: "DefaultCow",
+				pattern: "/{mooCount:int}",
+				defaults: new {controller="Home", action="DefaultCow"}
+			);
+
+			app.MapControllerRoute(
+				name: "NamedCow",
+				pattern: "/{mooCount:int}/{cowName}",
+				defaults: new {controller="Home", action="NamedCow"}
+			);
+
+			app.MapControllerRoute(
+				name: "AllCowsCount",
+				pattern: "/AllCows/Gallery/{cowCount:int}",
+				defaults: new {controller="Home", action="AllCowsCount"}
+			);
+
+			app.MapControllerRoute(
+				name: "AllCowsCountPerPage",
+				pattern: "/AllCows/Gallery/{cowCount:int}/Page{pageNumber:int}",
+				defaults: new {controller="Home", action="AllCowsCountPerPage"}
+			);
+
+			app.MapControllerRoute(
+				name: "AllCowsCountOnPage",
+				pattern: "/AllCows/Gallery/{cowCount:int}/{pageNumber:int}",
+				defaults: new {controller="Home", action="AllCowsCountOnPage"}
+			);
+
+			app.MapControllerRoute(
+				name: "TheFinalCatchAll",
+				pattern: "{*any}",
+				defaults: new {controller="Home", action="Index"}
+			);
+
+			app.Run();
+		}
+	}
 }
